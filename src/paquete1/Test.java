70cd7542e2f9e0ccb54760078d0bc9693c53b032
Pamela-Itzelt <a1245064@uabc.edu.mx>
2018-08-24 10:21:48 -0700

    Agregue ciclo foreach

--- src/paquete1/Test.java
+++ src/paquete1/Test.java
@@ -8,16 +8,25 @@ public class Test {
 		
 		double a=2;
 		double b=4;
-		Operaciones ope=Operaciones.SUMA;
 		double res=Operaciones.SUMA.resultado(a,b);
 		System.out.println(res);
 		System.out.println("Hola mundo");
+		
+		for(Operaciones ope:Operaciones.values()){
+			System.out.println(ope.name());
+		}
+		
+//		Operaciones ope[]=Operaciones.values();
+//		for(int i=0; i<ope.length;i++){
+//			System.out.println(ope[i].name());
+//		}
 	}
 
 }
 
 //MODO MENOS ELEGANTE
-/*double res=0;
+/*Operaciones ope=Operaciones.SUMA;
+ double res=0;
 if(ope==Operaciones.SUMA){
 	res= a+b;
 } else if(ope==Operaciones.RESTA){
